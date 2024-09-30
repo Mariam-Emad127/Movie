a59300fbe2a45703f900e41047cfb47006f10c1a
Mariam Emad <93050116+Mariam-Emad127@users.noreply.github.com>
2024-10-01 02:02:50 +0300

    now_playing_component

---
+++ lib/presentation/components/now_playing_component..dart
@@ -0,0 +1,153 @@
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fadein/flutter_fadein.dart';
+import 'package:movie/core/utils/enum.dart';
+import 'package:movie/presentation/controller/movies_bloc.dart';
+import 'package:movie/presentation/controller/movies_state.dart';
+
+import '../../core/network/api_constance.dart';
+ 
+
+class NowPlayingComponent extends StatelessWidget {
+  const NowPlayingComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MoviesBloc,MoviesState>(builder:(context,state) {
+      print(state.nowPlayingState);
+    
+switch(state.nowPlayingState){
+  case RequestState.loading:
+  return  const SizedBox(
+                height: 400.0,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+     
+     case RequestState.loaded:
+      
+      return FadeIn(
+        duration: const Duration(milliseconds: 500),
+        child: CarouselSlider(
+          options: CarouselOptions(
+            height: 400.0,
+            viewportFraction: 1.0,
+            onPageChanged: (index, reason) {},
+          ),
+          //items:moviesList.map(
+
+          items:state.nowPlayingMovies.map(
+          //moviesList.map(
+                (item) {
+                  print("kkkkkkkkkkkkkkkkkkkkkkk");
+              print(ApiConstance.imageUrl(item.backdropPath));
+              return GestureDetector(
+                key: const Key('openMovieMinimalDetail'),
+                onTap: () {
+                  /// TODO : NAVIGATE TO MOVIE DETAILS
+                },
+
+
+                child: Stack(
+                  children: [
+                    ShaderMask(
+                      shaderCallback: (rect) {
+                        return const LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            // fromLTRB
+                            Colors.transparent,
+                            Colors.black,
+                            Colors.black,
+                            Colors.transparent,
+                          ],
+                          stops: [0, 0.3, 0.5, 1],
+                        ).createShader(
+                          Rect.fromLTRB(0, 0, rect.width, rect.height),
+                        );
+                      },
+                      blendMode: BlendMode.dstIn,
+                      child: CachedNetworkImage(
+                        height: 560.0,
+                       
+                        imageUrl:
+          
+                        ApiConstance.imageUrl(item.backdropPath),
+                        
+           
+                        fit: BoxFit.cover,
+                      ),
+
+                      
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.circle,
+                                  color: Colors.redAccent,
+                                  size: 16.0,
+                                ),
+                                const SizedBox(width: 4.0),
+                                Text(
+                                  'Now Playing'.toUpperCase(),
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: Text(
+                              item.title!,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ).toList(),
+        ),
+
+      ) ;
+
+
+
+case RequestState.error:
+              return SizedBox(
+                height: 400.0,
+                child: Center(
+                  child: Text(state.message),
+                ),
+              );
+
+}
+
+
+
+
+    } );
+
+  }
+}
